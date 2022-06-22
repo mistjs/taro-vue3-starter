a09c11463a0d260aa66e25545be844981992ceeb
aibayanyu <aibayanyu@qq.com>
2022-06-23 07:16:21 +0800

    fix: useState change

--- src/composables/state.ts
+++ src/composables/state.ts
@@ -1,19 +1,11 @@
-import type { Ref } from "vue"
 import { ref } from "vue"
-interface UseStateType<T>{
-  state:Ref<T>;
-  setState:(value:T) => void;
-}
 
-
-export const useState = <T>(initialValue:T):UseStateType<T> => {
+export const useState = (initialValue:unknown) => {
   const state = ref(initialValue);
-  const setState = (value:T) => {
-    // @ts-ignore
+  const setState = (value:unknown) => {
     state.value = value;
   }
   return {
-    // @ts-ignore
     state,
     setState
   }
