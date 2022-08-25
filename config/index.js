898b5d946b673c5c6b06f681049002690ca72aba
zhuzhengjian <zhuzhengjian@hoteamsoft.com>
2022-08-25 16:19:51 +0800

    feat: init taro5.x

---
+++ config/index.js
@@ -0,0 +1,91 @@
+const config = {
+  projectName: 'aa',
+  date: '2022-8-25',
+  designWidth: 750,
+  deviceRatio: {
+    640: 2.34 / 2,
+    750: 1,
+    828: 1.81 / 2,
+  },
+  sourceRoot: 'src',
+  outputRoot: 'dist',
+  plugins: ['taro-plugin-pinia', '@tarojs/plugin-html'],
+  defineConstants: {
+  },
+  copy: {
+    patterns: [
+    ],
+    options: {
+    },
+  },
+  framework: 'vue3',
+  compiler: 'webpack5',
+  cache: {
+    enable: false, // Webpack 持久化缓存配置，建议开启。默认配置请参考：https://docs.taro.zone/docs/config-detail#cache
+  },
+  mini: {
+    webpackChain(chain) {
+      chain.merge({
+        module: {
+          rule: {
+            mjsScript: {
+              test: /\.mjs$/,
+              include: [/pinia/],
+              use: {
+                babelLoader: {
+                  loader: require.resolve('babel-loader'),
+                },
+              },
+            },
+          },
+        },
+      })
+    },
+    postcss: {
+      pxtransform: {
+        enable: true,
+        config: {
+
+        },
+      },
+      url: {
+        enable: true,
+        config: {
+          limit: 1024, // 设定转换尺寸上限
+        },
+      },
+      cssModules: {
+        enable: false, // 默认为 false，如需使用 css modules 功能，则设为 true
+        config: {
+          namingPattern: 'module', // 转换模式，取值为 global/module
+          generateScopedName: '[name]__[local]___[hash:base64:5]',
+        },
+      },
+    },
+  },
+  h5: {
+    publicPath: '/',
+    staticDirectory: 'static',
+    postcss: {
+      autoprefixer: {
+        enable: true,
+        config: {
+        },
+      },
+      cssModules: {
+        enable: false, // 默认为 false，如需使用 css modules 功能，则设为 true
+        config: {
+          namingPattern: 'module', // 转换模式，取值为 global/module
+          generateScopedName: '[name]__[local]___[hash:base64:5]',
+        },
+      },
+    },
+  },
+}
+
+module.exports = function(merge) {
+  if (process.env.NODE_ENV === 'development')
+    return merge({}, config, require('./dev'))
+
+  return merge({}, config, require('./prod'))
+}
