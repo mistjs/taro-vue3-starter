5f9c15ccb5af92197a070ecba35a34b45f41dff5
aibayanyu <aibayanyu@qq.com>
2023-04-29 23:11:19 +0800

    feat: add request demo

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -1,10 +1,18 @@
 <template>
   <view class="index p-40">
     <Counter />
+    <button @click="handleClick">
+      请求
+    </button>
+    {{ requestData }}
   </view>
 </template>
 
 <script lang="ts" setup>
 import './index.less'
-
+import { useGet } from '../../utils/request'
+const requestData = shallowRef()
+const handleClick = async() => {
+  requestData.value = await useGet('/test/200')
+}
 </script>
---
+++ src/utils/request.ts
@@ -0,0 +1,69 @@
+import type { AxiosError, AxiosRequestConfig, AxiosResponse, InternalAxiosRequestConfig } from 'axios'
+import axios from 'axios'
+
+const instance = axios.create({
+  baseURL: 'https://mock.28yanyu.cn/mock/642b5d1e939061307ed09d1b/example',
+  timeout: 60000,
+})
+
+const requestHandler = async(config: InternalAxiosRequestConfig): Promise<InternalAxiosRequestConfig> => {
+  // 新增缓存
+  // const token = useAuthorization()
+  // if (token.value) {
+  //   config.headers.set(STORAGE_AUTHORIZE_KEY, token.value)
+  // }
+  return config
+}
+
+export interface ResponseBody<T = any> {
+  code: number
+  data?: T
+  msg: string
+}
+const responseHandler = (response: any): ResponseBody<any> | AxiosResponse<any> | Promise<any> | any => {
+  return response.data
+}
+
+const errorHandler = (error: AxiosError): Promise<any> => {
+  return Promise.reject(error)
+}
+
+instance.interceptors.request.use(requestHandler, errorHandler)
+
+instance.interceptors.response.use(responseHandler, errorHandler)
+
+export const useGet = <T = any, R = any>(url: string, params?: T, config?: AxiosRequestConfig): Promise<ResponseBody<R>> => {
+  return instance.request({
+    url,
+    params,
+    method: 'GET',
+    ...config,
+  })
+}
+
+export const usePost = <T = any, R = any>(url: string, data?: T, config?: AxiosRequestConfig): Promise<ResponseBody<R>> => {
+  return instance.request({
+    url,
+    data,
+    method: 'POST',
+    ...config,
+  })
+}
+
+export const usePut = <T = any, R = any>(url: string, data?: T, config?: AxiosRequestConfig): Promise<ResponseBody<R>> => {
+  return instance.request({
+    url,
+    data,
+    method: 'PUT',
+    ...config,
+  })
+}
+
+export const useDelete = <T = any, R = any>(url: string, data?: T, config?: AxiosRequestConfig): Promise<ResponseBody<R>> => {
+  return instance.request({
+    url,
+    data,
+    method: 'DELETE',
+    ...config,
+  })
+}
